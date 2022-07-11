dff3a91494ef2f4eacd4181c016e5dc488543bb1
Mior Muhammad Zaki <crynobone@gmail.com>
2022-07-11 16:26:21 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Http/Controllers/DownloadsController.php
+++ src/Http/Controllers/DownloadsController.php
@@ -16,7 +16,7 @@ class DownloadsController extends Controller
      */
     public function __invoke(Request $request)
     {
-        abort_unless($request->filled(['disk', 'filename']), 404);
+        abort_unless($request->filled(['filename']), 404);
 
         // Dispatch deleting file after download.
 
